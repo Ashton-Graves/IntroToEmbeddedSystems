e4e778bff594e691f03010c9d44ea97130927c12
Qhoang5804 <qhoang5804@gmail.com>
2025-05-08 23:30:04 -0700

    attempted task b

--- Lab3/Lab3tb_Inits.h
+++ Lab3/Lab3tb_Inits.h
@@ -6,8 +6,8 @@
 // this header file, and thus you should not modify it for task 1a.
 // However, you may modify it if you want to reuse it for other tasks.
 
-#ifndef _Lab3_Inits_H_
-#define _Lab3_Inits_H_
+#ifndef _Lab3tb_Inits_H_
+#define _Lab3tb_Inits_H_
 
 // Preset frequency for 120 MHZ, 60 MHZ, and 12 MHZ clock
 // Pass in the preset to Timer1_Init and PLL_Init to configure
@@ -22,6 +22,9 @@ int PLL_Init(enum frequency freq);
 // Initializes the 4 onboard LEDs.
 void LED_Init(void);
 
+// Initializes the 2 onboard Switches.
+void Switch_Init(void);
+
 // Initializes ADC Module 0 Sample Sequencer 3. The module is triggered by
 // Timer module 0 subtimer A at 1 HZ. The interupt of ADC0 SS3 will be
 // generated whenever the A/D conversion is done.
@@ -30,4 +33,4 @@ void ADCReadPot_Init(void);
 // Initializes Timer 0A to trigger ADC0 at 1 HZ.
 void TimerADCTriger_Init(void);
 
-#endif  // _Lab3_Inits_H_
+#endif  // _Lab3tb_Inits_H_
