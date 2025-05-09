020a28c97fd39017c6713c99b9d24e307829e257
Qhoang5804 <qhoang5804@gmail.com>
2025-05-08 22:26:57 -0700

    added files

---
+++ Lab3/lab3t1b.h
@@ -0,0 +1,122 @@
+/*
+* Ashton Graves, Quan Hoang
+* graveash, qhoang05
+* 05/01/25
+
+* The purpose of this file is to define registers tailored to the functions we want for lab2
+*/
+
+#ifndef __HEADER2_H__
+#define __HEADER2_H__
+
+// GPIO Clock Gating
+#define RCGCGPIO (*((volatile uint32_t *)0x400FE608))
+// Timer Clock Gating
+#define RCGCTIMER (*((volatile uint32_t *)0x400FE604))
+
+
+// GPIO Port F
+#define GPIODIR_F (*((volatile uint32_t *)0x4005D400))
+#define GPIODEN_F (*((volatile uint32_t *)0x4005D51C))
+#define GPIODATA_F (*((volatile uint32_t *)0x4005D3FC))
+
+
+// GPIO Port N
+#define GPIODIR_N (*((volatile uint32_t *)0x40064400))
+#define GPIODEN_N (*((volatile uint32_t *)0x4006451C))
+#define GPIODATA_N (*((volatile uint32_t *)0x400643FC))
+
+
+// GPIO Port E
+#define GPIOAMSEL_E (*((volatile uint32_t *)0x4005C528))
+#define GPIODIR_E (*((volatile uint32_t *)0x4005C400))
+#define GPIODEN_E (*((volatile uint32_t *)0x4005C51C))
+#define GPIOAFSEL_E (*((volatile uint32_t *)0x4005C420))
+#define GPIODATA_E (*((volatile uint32_t *)0x4005C3FC))
+#define GPIOAFSEL_E (*((volatile uint32_t *)0x4005C420)) // enable port E alternative functions by bit
+#define GPIOAMSEL_E (*((volatile uint32_t *)0x4005C528))
+
+
+// PORT E input/interrupts
+#define GPIOIEV_E (*((volatile uint32_t *)0x4005C40C))  // Interrupt on falling edges or low levels for 0, rising edges or high edges for 1 ...
+#define GPIOIS_E (*((volatile uint32_t *)0x4005C404))  //  Selects between interrupt on edge (0) or level (1)
+#define GPIOIM_E (*((volatile uint32_t *)0x4005C410)) // Enable interrupts for port E. Enabling a bit 0-7 corresponds to enabling that port.
+#define GPIOICR_E (*((volatile uint32_t *)0x4005C41C)) // Clearing port E interrupt. Enabling a bit corresponds to clearing the interrupt for that port
+#define GPIOIBE_E (*((volatile uint32_t *)0x4005C408)) // detects interrupts on both edges
+
+
+// GPIO Port J
+#define GPIODIR_J (*((volatile uint32_t *)0x40060400))
+#define GPIODEN_J (*((volatile uint32_t *)0x4006051C))
+#define GPIOPUR_J (*((volatile uint32_t *)0x40060510))
+#define GPIODATA_J (*((volatile uint32_t *)0x400603FC))
+#define GPIOIEV_J (*((volatile uint32_t *)0x4006040C)) // Interrupt on falling edges or low levels for 0, rising edges or high edges for 1 ...
+#define GPIOIS_J (*((volatile uint32_t *)0x40060404)) //  Selects between interrupt on edge (0) or level (1)
+#define GPIOIM_J (*((volatile uint32_t *)0x40060410)) // Enable interrupts for port J. Enabling a bit 0-7 corresponds to enabling that port.
+#define GPIOICR_J (*((volatile uint32_t *)0x4006041C)) // Clearing port J interrupt. Enabling a bit corresponds to clearing the interrupt for that port
+
+
+// Timer 0A Registers
+#define GPTMCTL_0 (*((volatile uint32_t *)0x4003000C)) // Enable/disable timer
+#define GPTMCFG_0 (*((volatile uint32_t *)0x40030000)) // 32 bit mode
+#define GPTMTAMR_0 (*((volatile uint32_t *)0x40030004)) // Set mode (periodic, countdown)
+#define GPTMTAILR_0 (*((volatile uint32_t *)0x40030028)) // Set threshold
+#define GPTMIMR_0 (*((volatile uint32_t *)0x40030018)) // GPTM interrupt mask
+#define GPTMADCEV_0 (*((volatile uint32_t *)0x40030070)) // GPTM ADC event enable
+
+
+// Flag polling
+#define GPTMRIS_0 (*((volatile uint32_t *)0x4003001C))
+#define GPTMICR_0 (*((volatile uint32_t *)0x40030024)) // GPTM Timer0 interrupt clear
+
+
+// Timer 1A Registers
+#define GPTMCTL_1 (*((volatile uint32_t *)0x4003100C))
+#define GPTMCFG_1 (*((volatile uint32_t *)0x40031000))
+#define GPTMTAMR_1 (*((volatile uint32_t *)0x40031004))
+#define GPTMTAILR_1 (*((volatile uint32_t *)0x40031028))
+#define GPTMIMR_1 (*((volatile uint32_t *)0x40031018)) // GPTM 1 interrupt mask
+    
+
+// Flag polling
+#define GPTMRIS_1 (*((volatile uint32_t *)0x4003101C))
+#define GPTMICR_1 (*((volatile uint32_t *)0x40031024))  // GPTM Timer1 interrupt clear
+
+
+// Timer 2A Registers
+#define GPTMCTL_2 (*((volatile uint32_t *)0x4003200C))
+#define GPTMCFG_2 (*((volatile uint32_t *)0x40032000))
+#define GPTMTAMR_2 (*((volatile uint32_t *)0x40032004))
+#define GPTMTAILR_2 (*((volatile uint32_t *)0x40032028))
+#define GPTMIMR_2 (*((volatile uint32_t *)0x40032018)) // GPTM 2 interrupt mask
+
+    
+
+// Flag polling
+#define GPTMRIS_2 (*((volatile uint32_t *)0x4003201C))
+#define GPTMICR_2 (*((volatile uint32_t *)0x40032024))  // GPTM Timer2 interrupt clear
+
+
+// Interrupts
+#define EN0 (*((volatile uint32_t *)0xE000E100)) // Enable interrupt (0-31)
+#define EN1 (*((volatile uint32_t *)0xE000E104)) // Enable interrupt (32-63)
+
+
+// ADC
+#define RCGCADC (*((volatile uint32_t *)0x400FE638)) // ADC clock gating
+#define ADCCC_0 (*((volatile uint32_t *)0x40038FC8)) // ADC clock configuration
+#define ALTCLKCFG (*((volatile uint32_t *)0x400FE138)) // set alternative clock source
+#define ADCACTSS_0 (*((volatile uint32_t *)0x40038000)) // turn on/off ADC0 SS3
+#define ADCEMUX_0 (*((volatile uint32_t *)0x40038014)) // selects the event (trigger) that initiates sampling for each sample sequencer
+#define ADCSSEMUX3_0 (*((volatile uint32_t *)0x400380B8)) // chooses which group of analog inputs
+#define ADCSSMUX3_0 (*((volatile uint32_t *)0x400380A0)) // chooses which analog input
+#define ADCSSCTL3_0 (*((volatile uint32_t *)0x400380A4)) // configure ss3
+#define ADCIM_0 (*((volatile uint32_t *)0x40038008)) // ADC0 interrupt mask
+#define ADCISC_0 (*((volatile uint32_t *)0x4003800C)) // ADC0 interrupt clear
+#define ADCSSFIFO3_0 (*((volatile uint32_t *)0x400380A8)) // ADC0 SS3 result (FIFO)
+#define ADCRIS_0 (*((volatile uint32_t *)0x40038004)) // ADC0 Interrupt status
+#define ADCPSSI_0 (*((volatile uint32_t *)0x40038028)) // ADC0 Sampler Init
+
+
+#endif //__HEADER2_H__
+
--- Lab3/task1b/main1b.c
+++ Lab3/task1b/main1b.c
@@ -1,5 +1,55 @@
+/**
+ * EE/CSE 474: Lab3 Task1b main function
+*/
 
-int main()
-{
+#include <stdint.h>
+#include "Lab3_Inits.h"
+#include "lab3t1a.h"
+// STEP 0b: Include your header file here
+// YOUR CUSTOM HEADER FILE HERE
+
+uint32_t ADC_value;
+
+int main(void) {
+  // Select system clock frequency preset
+  enum frequency freq = PRESET2; // 60 MHz
+  PLL_Init(freq);        // Set system clock frequency to 60 MHz
+  LED_Init();            // Initialize the 4 onboard LEDs (GPIO)
+  ADCReadPot_Init();     // Initialize ADC0 to read from the potentiometer
+  TimerADCTriger_Init(); // Initialize Timer0A to trigger ADC0
+  float resistance;
+  while(1) {
+    // STEP 5: Change the pattern of LEDs based on the resistance.
+    // 5.1: Convert ADC_value to resistance in kilo-ohm
+    resistance = (ADC_value / 4095.0) * 10.0;
+    // 5.2: Change the pattern of LEDs based on the resistance
+    if (resistance < 2.5) {
+      GPIODATA_N |= 0x2; // Set PN1 to 1
+      GPIODATA_N &= ~0x1;
+      GPIODATA_F &= ~(0x11);
+      // GPIODATA_F = 0x0;
+    } else if (resistance < 5.0) {
+      GPIODATA_N |= 0x3; // Set PN1 and PN0 to 1
+      GPIODATA_F &= ~(0x11);
+      // GPIODATA_F = 0x0;
+    } else if (resistance < 7.5) {
+      GPIODATA_F |= 0x10; // Set PF4 to 1
+      GPIODATA_F &= ~(0x1);
+    } else {
+      GPIODATA_F |= 0x11; // Set PF4 and PF0 to 1
+    }
+  }
   return 0;
+}
+
+void ADC0SS3_Handler(void) {
+  // STEP 4: Implement the ADC ISR.
+  // 4.1: Clear the ADC0 interrupt flag
+  ADCISC_0 |= 0x8; 
+  
+  // 4.2: Save the ADC value to global variable ADC_value
+  ADCPSSI_0 |= 0x8; // Starts the conversion process
+  
+  while((ADCRIS_0 & 0x8) != 0x0) {} // waits for the conversion to finish
+  ADC_value = ADCSSFIFO3_0; // get 12 ADC result bits
 }
