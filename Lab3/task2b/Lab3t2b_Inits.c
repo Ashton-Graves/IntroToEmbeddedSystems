969b213766e6135d239ec82e5d15a143aff786fb
Ashton Graves <ashtongraves@Ashtons-MacBook-Air.local>
2025-05-15 19:26:04 -0700

    finished task 2b

--- Lab3/task2b/Lab3t2b_Inits.c
+++ Lab3/task2b/Lab3t2b_Inits.c
@@ -5,26 +5,6 @@
 #include "PLL_Header.h"
 #include "Lab3t2b_Inits.h"
 #include "lab3t2b.h"
-
-void LED_Init(void) {
-  // STEP 1: Initialize the 4 on board LEDs by initializing the corresponding
-  // GPIO pins.
-
-  // YOUR CODE HERE
-  RCGCGPIO |= 0x1000 | 0x20; // Enable PortN & PortF GPIO
-  
-  volatile int delay = 0;
-  delay++;
-  delay++;
-  
-  GPIODIR_F = 0x11; // Set PF0 and PF4 to output
-  GPIODEN_F = 0x11; // Set PF0 and PF4 to digital port
-  GPIODATA_F = 0x0; 
-   
-  GPIODIR_N = 0x3; // Set PN0 and PN1 to output
-  GPIODEN_N = 0x3; // Set PN0 and PN1 to digital port
-  GPIODATA_N = 0x0; // Set Port N to 0s
-}
    
    
 void UART_Init(void) {
@@ -47,10 +27,16 @@ void UART_Init(void) {
   UARTFBRD_2 = 11; // fractional baud rate divisor
 
   UARTLCRH_2 = 0x60; // 8 bit word length, no parity, one stop bit
-  UARTCTL_2 |= 0x301; // enable transmit, recieve section of UART, and enable UART
   
-  UARTIM_2 |= 0x30; // set TXIM, RXIM bit to cause interrupt to be sent to NVIC when transmit, recieve is finished
+  UARTIM_2 |= 0x10; // set TXIM, RXIM bit to cause interrupt to be sent to NVIC when transmit, recieve is finished
+  
+  // Flush RX FIFO
+  while ((UARTFR_2 & 0x10) == 0) {
+    volatile char _ = UARTDR_2;
+  }
 
   UARTICR_2 = 0x30; // use this in interrupt handler for interrupt clear
   EN1 |= 0x2; // enable interrupt 33 (UART2)
+  
+  UARTCTL_2 |= 0x301; // enable transmit, recieve section of UART, and enable UART
 }
--- Lab3/task2b/main2b.c
+++ Lab3/task2b/main2b.c
@@ -15,25 +15,20 @@ int main()
   LED_Init();
   UART_Init();
 
-  while(1) {
-  }
+  while(1) {}
   return 0;
 }
-
 
 #pragma call_graph_root = "interrupt"
 __weak void UART2_Handler( void ) {
   if ((UARTMIS_2 & 0x30) == 0x10) {       // Receive Interrupt
-    GPIODATA_F ^= 0x1;
-    UARTICR_2 = 0x10; // Clear receive interrupt bit
-    c = UARTDR_2 & 0xFF;
-    while ((UARTFR_2 & 0x20)); // while until the last frame is done transmitting
-    UARTDR_2 = c;
-    printf("receive interrupt");
-  } else if ((UARTMIS_2 & 0x30) == 0x20) {        // Transmit Interrupt
-    UARTICR_2 = 0x20; // Clear recieve interrupt bit
-    printf("transmit interrupt");
+    while ((UARTFR_2 & 0x10) == 0) {  // While RX FIFO not empty
+      UARTICR_2 = 0x10; // Clear receive interrupt bit
+      c = UARTDR_2 & 0xFF;
+      while ((UARTFR_2 & 0x20)); // while until the last frame is done transmitting
+      UARTDR_2 = c;
+    }
   } else {
-    printf("neither");
+    UARTICR_2 = 0x30; // Clear receive interrupt bit
   }
 }
