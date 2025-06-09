3d1c3dc086fcd5b94b51aa05718a8acd266d3a62
Ashton Graves <ashtonmichael19@gmail.com>
2025-06-08 22:38:14 -0700

    arduino updates for working Lab5

--- Lab5/arduino_daisy/lab5/lab5.ino
+++ Lab5/arduino_daisy/lab5/lab5.ino
@@ -1,51 +1,55 @@
 #include "DaisyDuino.h"
 #include <Wire.h>
+#include "daisyDSP.h"  // Add this
+using namespace daisysp;
+
+ReverbSc verb;
  
 byte RxByte;
 
 DaisyHardware hw;
 static Oscillator osc;
-float pitch_knob;
 
 size_t num_channels;
 
-float notes[12] = {
-  32.70375f,
-  //34.6475f,
-  36.7075f,
-  38.89125f,
-  //41.20375f,
-  43.65375f,
-  //46.24875f,
-  49.0f,
-  51.9125f,
-  //55.0f,
-  58.27f,
-  //61.735f,
-  65.4075f
+float notes[13] = {
+174.61f,
+195.99f,
+219.99f,
+233.07f,
+261.62f,
 };
 
 int note_index;
 
 void MyCallback(float **in, float **out, size_t size) {
   float sine_signal;
-  osc.SetFreq(notes[note_index] * pow(2.0, 5.0/12.0));
+  float wet_l, wet_r;
   for (size_t i = 0; i < size; i++) {
     sine_signal = osc.Process();
-    out[0][i] = sine_signal;
-    out[1][i] = sine_signal; 
+    verb.Process(sine_signal, sine_signal, &wet_l, &wet_r);
+    //out[0][i] = sine_signal;
+    //out[1][i] = sine_signal; 
+    out[0][i] = 0.7f * sine_signal + 0.3f * wet_l;
+    out[1][i] = 0.7f * sine_signal + 0.3f * wet_r;
   }
 }
 
 void setup() {
-  // Initialize seed at 48kHz
-  Wire.begin(0x55); // Initialize I2C (Slave Mode: address=0x55 )
+  Serial.begin(9600);
+  Wire.begin(0x08); // Initialize I2C (Slave Mode: address=0x55 )
   Wire.onReceive(I2C_RxHandler);
+  // Initialize seed at 48kHz
   float sample_rate;
   hw = DAISY.init(DAISY_SEED, AUDIO_SR_48K);
   num_channels = hw.num_channels;
   sample_rate = DAISY.get_samplerate();
   osc.Init(sample_rate);
+
+    // initialize reverb
+  verb.Init(DAISY.get_samplerate());
+  verb.SetFeedback(0.85f);  // Range: 0 to ~0.95
+  verb.SetLpFreq(8000.0f); // Low-pass filter frequency
 
   // Set the parameters for oscillator 
   osc.SetWaveform(osc.WAVE_SAW);
@@ -57,17 +61,25 @@ void setup() {
 }
 
 void loop() {
-  analogReadResolution(16);
-  int raw = analogRead(A0);             // 0–4095
-  note_index = raw / (pow(2.0, 16) / 8);       // Quantize to 0–11
-  note_index = constrain(note_index, 0, 7); // Just in case
-
+  Serial.println(" * * * * * * * * * * * *");
+  delay(3000);
 }
 
 void I2C_RxHandler(int numBytes)
 {
+  Serial.println("message recieved");
   while(Wire.available()) {  // Read Any Received Data
     RxByte = Wire.read();
+    Serial.print("RxByte: ");
+    Serial.println(RxByte);
+    if (RxByte == 0) {
+      osc.SetAmp(0);
+    } else {
+      osc.SetFreq(notes[RxByte-1]);
+      osc.SetAmp(0.1);
+      Serial.println("note");
 
+    }
   }
 }
+
